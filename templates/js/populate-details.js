2fc782b76c711d8a9cb032f92b65117d4d9e9a2d
Paul Iacomi <pauliacomi@users.noreply.github.com>
2019-12-09 23:37:59 +0100

    use global replacement

--- templates/js/populate-details.js
+++ templates/js/populate-details.js
@@ -2,9 +2,9 @@ var indices = cb_data.source.selected.indices;
 if (indices.length > 0) {
 
     const idx = indices[0];
-    tp = tp.replace('@ID', cb_data.source.data['labels'][idx]);
-    tp = tp.replace('@temp', cb_data.source.data['temp'][idx]);
-    tp = tp.replace('@doi', cb_data.source.data['doi'][idx]);
+    tp = tp.replace(/@ID/g, cb_data.source.data['labels'][idx]);
+    tp = tp.replace(/@temp/g, cb_data.source.data['temp'][idx]);
+    tp = tp.replace(/@doi/g, cb_data.source.data['doi'][idx]);
 
     var det = document.getElementById('iso-details')
     det.style.display = 'flex';
