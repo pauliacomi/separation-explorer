d5fcf2e8a7e2fc5ef106d98494808b5e75482ed2
Paul Iacomi <pauliacomi@users.noreply.github.com>
2020-01-17 16:30:28 +0100

    change default gas order

--- src/datastore.py
+++ src/datastore.py
@@ -9,8 +9,8 @@ DATASET = None          # Entire dataset
 INITIAL = None          # An example initial dataset
 PROBES = None           # Probes in the initial dataset
 SETTINGS = {
-    'g1': 'carbon dioxide',
-    'g2': 'methane',
+    'g1': 'methane',
+    'g2': 'carbon dioxide',
     't_abs': 303,
     't_tol': 5,
 }
